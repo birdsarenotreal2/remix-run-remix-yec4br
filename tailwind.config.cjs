1d86ec3507797352b4f0a8fabe01f0084ec32202
birdsarenotreal2 <samfortierbruh@gmail.com>
2024-10-26 22:28:22 -0400

    Add files via upload

---
+++ tailwind.config.cjs
@@ -0,0 +1,27 @@
+const { resolveProjectPath } = require('wasp/dev')
+const colors = require('tailwindcss/colors')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    resolveProjectPath('./src/**/*.{js,jsx,ts,tsx}'),
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          50:  colors.purple[50],
+          100: colors.purple[100],
+          200: colors.purple[200],
+          300: colors.purple[300],
+          400: colors.purple[400],
+          500: colors.purple[500],
+          600: colors.purple[600],
+          700: colors.purple[700],
+          800: colors.purple[800],
+          900: colors.purple[900],
+        }
+      }
+    },
+  },
+}
